b7d96254fb163f1d29eb175bc0bc95f8867044f1
gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-06-19 11:25:49 -0300

    Add files via upload

---
+++ js/bash.js
@@ -0,0 +1,9 @@
+console.log("testando");
+
+$("doucument").ready(function () {
+  var typed = new Typed("#typed", {
+    stringsElement: "#typed-strings",
+    backSpeed: 0,
+    typeSpeed: 60,
+  });
+});
